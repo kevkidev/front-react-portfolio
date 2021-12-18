c755ad7420f740461ed9d43b97db30b3c38ce6d7
kevkidev <>
2021-12-18 16:07:47 +0100

    add basic routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFoundPage from "./main/NotFoundPage";
+import Login from "./main/Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+
+        <Route path="/:login" element={<Login />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/main/NotFoundPage.js
+++ src/main/NotFoundPage.js
@@ -1,16 +1,12 @@
 import { Link } from "react-router-dom";
 import { Fragment } from "react/cjs/react.production.min";
-import "./App.css";
 
-function App() {
+function NotFoundPage() {
   return (
     <Fragment>
-      <nav>
-        <Link to="/">Kevkidev</Link>
-      </nav>
-      <h1>Ca marche :)</h1>
+      <h1>Oups! your are lost ?</h1>
+      <Link to="/">Return</Link>
     </Fragment>
   );
 }
-
-export default App;
+export default NotFoundPage;

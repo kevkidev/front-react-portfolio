1a0db3111e9247b9acbf0c8d3d21b5fee6adef22
kevkidev <unknown>
2022-01-03 20:33:22 +0100

    fix routing

--- src/common/routes.js
+++ src/common/routes.js
@@ -1,2 +1,8 @@
 export const PATH_ROOT = "/" + process.env.REACT_APP_NAME;
 export const PATH_MARKDOWN_EDITOR = PATH_ROOT + "/projects/markdown-editor";
+export const PATH_PROJECTS = "/projects";
+export const PATH_PROJECTS_TIME_INTERVAL = PATH_PROJECTS + "/time-interval";
+export const PATH_PROJECTS_CALCULATOR = PATH_PROJECTS + "/calculator";
+export const PATH_PROJECTS_DRUM_MACHINE = PATH_PROJECTS + "/drum-machine";
+export const PATH_PROJECTS_CHAT = PATH_PROJECTS + "/chat";
+export const PATH_PROJECTS_GAME_BLOCKS = PATH_PROJECTS + "/game-blocks";

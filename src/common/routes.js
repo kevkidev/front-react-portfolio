e21ba3773d7b208fe642a137b6333385166095ca
kevkidev <>
2022-01-04 12:32:11 +0100

    Fix routes basename

--- src/common/routes.js
+++ src/common/routes.js
@@ -1,6 +1,7 @@
-export const PATH_ROOT = "/" + process.env.REACT_APP_NAME;
-export const PATH_MARKDOWN_EDITOR = PATH_ROOT + "/projects/markdown-editor";
-export const PATH_PROJECTS = "/projects";
+export const PATH_ROOT = "/";
+export const PATH_BASENAME = PATH_ROOT + process.env.REACT_APP_NAME;
+export const PATH_PROJECTS = PATH_ROOT + "projects";
+export const PATH_MARKDOWN_EDITOR = PATH_PROJECTS + "/markdown-editor";
 export const PATH_PROJECTS_TIME_INTERVAL = PATH_PROJECTS + "/time-interval";
 export const PATH_PROJECTS_CALCULATOR = PATH_PROJECTS + "/calculator";
 export const PATH_PROJECTS_DRUM_MACHINE = PATH_PROJECTS + "/drum-machine";
